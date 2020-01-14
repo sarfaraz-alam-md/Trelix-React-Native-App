a193a76c50e5b907c42b5c872c9f0d4d62fe698a
Ubuntu <ubuntu@ip-172-31-93-255.ec2.internal>
2020-01-14 15:48:40 +0000

    initial commit added my chatbot native app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ChatBot } from 'aws-amplify-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ handleComplete(err, confirmation) {
+    if (err) {
+      alert('Bot conversation failed')
+      return;
+    }
+    alert('Success: ' + JSON.stringify(confirmation, null, 2));
+    return 'Thank you! What would you like to do next?';
+  }
+  
+  render() {
+    return (
+      <div className="App">
+       <ChatBot
+         title="Trelix ChatBot"
+         botName="QnABotWorkshop_BotFe"
+         welcomeMessage="Welcome, how can I help you today?"
+         onComplete={this.handleComplete.bind(this)}
+         clearOnComplete={true}
+        />
+      </div>
+    );
+  }
 }
+
+const styles = {
+  bubbleStyles: {
+    text: {
+      fontSize: 16,
+    },
+    chatbubble: {
+      borderRadius: 30,
+      padding: 10
+    }
+  },
+  headerTitle: {
+    color: 'white',
+    fontSize: 22
+  },
+  header: {
+    backgroundColor: 'rgb(0, 132, 255)',
+    padding: 20,
+    borderTop: '12px solid rgb(204, 204, 204)'
+  },
+  messagesContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 10,
+    alignItems: 'center'
+  },
+  input: {
+    fontSize: 16,
+    padding: 10,
+    outline: 'none',
+    width: 350,
+    border: 'none',
+    borderBottom: '2px solid rgb(0, 132, 255)'
+  }
+}
+  
+  
 
 export default App;
